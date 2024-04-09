5cc743f348da4ab06ad4daca640dd465498ead69
Tuon13141 <ngteemo22@gmail.com>
2024-04-09 19:54:09 +0700

    update 9/4/2024 (5)

---
+++ MoneyChange/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so dola can doi : ");
+        float dola = float.Parse(Console.ReadLine());
+
+        float vnd = dola * 23000;
+        Console.WriteLine("So tien vnd sau khi doi : " + vnd);
+    }
+}
