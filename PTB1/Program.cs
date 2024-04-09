baff184c6e1de1b8988386577e25919c3e7f9082
Tuon13141 <ngteemo22@gmail.com>
2024-04-09 19:58:23 +0700

    update 9/4/2024 (6)

---
+++ PTB1/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    public class BPTB1
+    {
+        int a;
+        int b;
+
+        public BPTB1(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public void Solve()
+        {
+            if(a == 0)
+            {
+                if(b == 0)
+                {
+                    Console.WriteLine("Vo so nghiem");
+                }
+                else
+                {
+                    Console.WriteLine("Vo nghiem");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nghiem x = " + CalX());
+            }
+        }
+        public float CalX()
+        {
+            return -(float)b / a;
+        }
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("a.x + b = 0");
+        Console.WriteLine("Nhap a : ");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap b : ");
+        int b = int.Parse(Console.ReadLine());
+
+        BPTB1 bpt = new BPTB1(a, b);
+        bpt.Solve();
+    }
+}
