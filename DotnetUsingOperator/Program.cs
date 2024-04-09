dc1a3885cfb71ea8b2ba82cdf8cee09635d04e43
Tuon13141 <ngteemo22@gmail.com>
2024-04-09 19:48:44 +0700

    update 9/4/2024 (3)

---
+++ DotnetUsingOperator/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter width: ");
+        float width = float.Parse(Console.ReadLine());
+        Console.Write("Enter height: ");
+        float height = float.Parse(Console.ReadLine());
+
+        float area = width * height;
+        Console.WriteLine("Area is: " + area);
+    }
+}
