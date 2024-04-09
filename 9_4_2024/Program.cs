eb6799fe99757688cdc993abe7307ca6717bb8c4
Tuon13141 <ngteemo22@gmail.com>
2024-04-09 19:18:13 +0700

    Update 4/9/2024 (1)

---
+++ 9_4_2024/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so luong phan tu trong mang : ");
+        int a = int.Parse(Console.ReadLine());
+        int[] b = new int[a];
+        int sum = 0;
+        for(int i = 0; i < a; i++){
+            Console.WriteLine("Nhap phan tu thu " + (i+1));
+            b[i] = int.Parse(Console.ReadLine());
+            sum+=b[i];
+        }
+        Console.WriteLine("Cac phan tu trong mang : ");
+        for(int i = 0; i < a; i++){
+            Console.Write(b[i] + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("Tong cua mang : " + sum);
+
+    }
+}
