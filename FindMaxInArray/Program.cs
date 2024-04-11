d664615ae4dbd2f6dd25069af10a7efff722075f
Tuon13141 <ngteemo22@gmail.com>
2024-04-11 18:14:53 +0700

    FindMaxInArray

---
+++ FindMaxInArray/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so luong phan tu trong mang : ");
+        int a = int.Parse(Console.ReadLine());
+        int[] b = new int[a];
+        int max = 0;
+        int index = 0;
+        for(int i = 0; i < a; i++){
+            Console.WriteLine("Nhap phan tu thu " + (i+1));
+            b[i] = int.Parse(Console.ReadLine());
+            if(max <= b[i]){
+                max = b[i];
+                index = i + 1;
+            }
+
+        }
+        Console.WriteLine("Cac phan tu trong mang : ");
+        for(int i = 0; i < a; i++){
+            Console.Write(b[i] + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("Phan tu Max cua mang : " + max + ", la phan tu thu : " + index);
+    }
+}
