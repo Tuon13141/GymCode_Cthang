f6a17652750e0a96b2952a787d3ac36a6a14fac3
Tuon13141 <ngteemo22@gmail.com>
2024-04-11 18:09:23 +0700

    FindNameInArray

---
+++ FindNameInArray/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string [] names = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+        Console.WriteLine("Nhap ten muon tim : ");
+        string findName = Console.ReadLine();
+        int index = 0;
+        foreach(string name in names){
+            index++;
+            if(findName.ToLower().Equals(name.ToLower())){              
+                Console.WriteLine("Da tim thay ten : " + name + ". La phan tu thu : " + index);
+                return;
+            }
+
+        }
+
+        Console.WriteLine("Khong ton tai ten do !");
+    }
+}
