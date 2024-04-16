7e9e602b1aa997c5e8f9996e5c7afc09cd301965
Tuon13141 <ngteemo22@gmail.com>
2024-04-16 17:04:57 +0700

    DegreeChanger

---
+++ DegreeChanger/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    public static double CelsiusToFahrenheit(double celsius)
+    {
+        double fahrenheit = (9.0 / 5) * celsius + 32;
+        return fahrenheit;
+    }
+    public static double FahrenheitToCelsius(double fahrenheit) {
+        double celsius = (5.0 / 9) * (fahrenheit - 32);
+        return celsius;
+    } 
+    private static void Main(string[] args)
+    {
+        double fahrenheit;
+        double celsius;
+        int choice;
+
+        do
+        {
+            Console.WriteLine("Menu.");
+            Console.WriteLine("1. Fahrenheit to Celsius");
+            Console.WriteLine("2. Celsius to Fahrenheit");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("Enter your choice: ");
+            choice = Int32.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1: 
+                break;
+                case 2: 
+                break;
+                case 0:
+                    Environment.Exit(0);
+                    break;
+            }
+        } while (choice != 0);
+    }
+}
