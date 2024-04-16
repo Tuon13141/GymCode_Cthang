6098f7c684677bbdbc602690e83d1702c539c33b
Tuon13141 <ngteemo22@gmail.com>
2024-04-16 17:35:54 +0700

    LetterCountInStringArray

---
+++ CountLetterFromStringArray/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    public static void CountLetter(char letter, string input){
+        int count = 0;
+        int index = 1;
+        foreach(char c in input){
+            if(letter == c){
+                count++;
+                Console.WriteLine("Phat hien ki tu trung o thu tu thu : " + index);
+            }
+            index++;
+        }
+
+        Console.WriteLine("Tong so ki tu da dem duoc : " + count);
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap chuoi string :");
+        string input = Console.ReadLine();
+        Console.WriteLine("Nhap ki tu can dem : ");
+        char letter = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+        CountLetter(letter, input);
+    }
+}
