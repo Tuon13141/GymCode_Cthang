639fe8aba4c10c5b2436c8518f860257e6e055b1
Tuon13141 <ngteemo22@gmail.com>
2024-04-25 17:57:16 +0700

    update

--- Icomparable/Program.cs
+++ Icomparable/Program.cs
@@ -35,25 +35,53 @@
             else return 1;
         }
     }
+
+    public class CircleComparator : IComparer<Circle>
+    {
+        public int Compare(Circle c1, Circle c2)
+        {
+            if (c1.getRadius() > c2.getRadius()) return 1;
+            else if (c1.getRadius() < c2.getRadius()) return -1;
+            else return 0;
+        }
+    }
     private static void Main(string[] args)
     {
         ComperableCircle[] circles = new ComperableCircle[3];
-      circles[0] = new ComperableCircle(3.6);
-      circles[1] = new ComperableCircle();
-      circles[2] = new ComperableCircle(3.5, "indigo", false);
+        circles[0] = new ComperableCircle(3.6);
+        circles[1] = new ComperableCircle();
+        circles[2] = new ComperableCircle(3.5, "indigo", false);
 
-      Console.WriteLine("Pre-sorted:");
-      foreach (ComperableCircle circle in circles)
-      {
+        Console.WriteLine("Pre-sorted:");
+        foreach (ComperableCircle circle in circles)
+        {
+                Console.WriteLine(circle.getRadius());
+        }
+
+        Array.Sort(circles);
+
+        Console.WriteLine("After-sorted:");
+        foreach (ComperableCircle circle in circles)
+        {
             Console.WriteLine(circle.getRadius());
-      }
+        }
+        circles[0] = new ComperableCircle(3.6);
+        circles[1] = new ComperableCircle();
+        circles[2] = new ComperableCircle(3.5, "indigo", false);
 
-      Array.Sort(circles);
-
-      Console.WriteLine("After-sorted:");
-      foreach (ComperableCircle circle in circles)
-      {
+        Console.WriteLine("Pre-sorted:");
+        foreach (Circle circle in circles)
+        {
             Console.WriteLine(circle.getRadius());
-      }
+        }
+
+        IComparer<Circle> circleComparator = new CircleComparator();
+        Array.Sort(circles, circleComparator);
+
+        Console.WriteLine("After-sorted:");
+        foreach (Circle circle in circles)
+        {
+            Console.WriteLine(circle.getRadius());
+        }
     }
 }
